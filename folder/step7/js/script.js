9ebefd078391e1b57fafc053c9928b17d3bd7142
gloria <Gloriab@MacBook-Pro-3.local>
2018-02-27 18:58:55 -0800

    cv

--- folder/step7/js/script.js
+++ folder/step7/js/script.js
@@ -2,30 +2,30 @@ $(document).ready(function(){
 
 	// try uncommenting (deleting the // before a line) to see its effects
 
-	$('.item').addClass('shrink');	
+	$('.item').addClass('shrink');
 
-	$('body').click(function() {
-		$(this).toggleClass('colorful');
-	});	
+	//$('body').click(function() {
+	//	$(this).toggleClass('colorful');
+	//});	
 
 	/* round corners on click */
 
 	// $('body').click(function() {
 	// 	$('.item').toggleClass('round');
-	// });	
+	// });
 
 	/* hover to rotate */
 
 	// $('.item').hover(function() {
 	// 	$(this).toggleClass('rotate');
-	// });	
+	// });
 
 	/* clone an element */
 
 	// $('.item').click(function() {
 	// 	var item = $(this).clone();
 	// 	$('#grid').append(item);
-	// });	
+	// });
 
 	/* remove an element */
 
@@ -33,15 +33,15 @@ $(document).ready(function(){
 	// 	$(this).remove();
 	// });
 
-	/* random backgrounds over time */		
+	/* random backgrounds over time */
 
 	// setInterval(function(){
 	// 	$redValue = Math.floor(Math.random()*255);
 	// 	$greenValue = Math.floor(Math.random()*255);
-	// 	$blueValue = Math.floor(Math.random()*255);	
+	// 	$blueValue = Math.floor(Math.random()*255);
 	// 	$('.item').css(
 	// 		'background', 'rgba('+$redValue+','+$blueValue+','+$greenValue+', 1)'
 	// 	);
 	// },500);
-	
+
 });
