da972dd96cbbce1b90a771865176bbabee46c0a8
gloria <Gloriab@MacBook-Pro-3.local>
2018-03-04 11:13:03 -0800

    3

--- folder/step7/js/script.js
+++ folder/step7/js/script.js
@@ -1,15 +1,126 @@
 $(document).ready(function(){
 
-	$('#btn').hover(
+
+
+	$('#img1').hover(
 	    function() {
-	        $('#img1').hide();
+	        $('#btn').show();
 	    },function() {
-	        $('#img1').show();
+	        $('#btn').hide();
 	    }
 	);
 
+	$('#img2').hover(
+			function() {
+					$('#btn2').show();
+			},function() {
+					$('#btn2').hide();
+			}
+	);
 
+	$('#img3').hover(
+			function() {
+					$('#btn3').show();
+			},function() {
+					$('#btn3').hide();
+			}
+	);
 
+	$('#img4').hover(
+			function() {
+					$('#btn4').show();
+			},function() {
+					$('#btn4').hide();
+			}
+	);
+
+	$('#img5').hover(
+			function() {
+					$('#btn5').show();
+			},function() {
+					$('#btn5').hide();
+			}
+	);
+
+	$('#img6').hover(
+			function() {
+					$('#btn6').show();
+			},function() {
+					$('#btn6').hide();
+			}
+	);
+
+	$('#img7').hover(
+			function() {
+					$('#btn7').show();
+			},function() {
+					$('#btn7').hide();
+			}
+	);
+
+	$('#img8').hover(
+			function() {
+					$('#btn8').show();
+			},function() {
+					$('#btn8').hide();
+			}
+	);
+
+	$('#img9').hover(
+			function() {
+					$('#btn9').show();
+			},function() {
+					$('#btn9').hide();
+			}
+	);
+
+	$('#img10').hover(
+			function() {
+					$('#btn10').show();
+			},function() {
+					$('#btn10').hide();
+			}
+	);
+
+	$('#img11').hover(
+			function() {
+					$('#btn11').show();
+			},function() {
+					$('#btn11').hide();
+			}
+	);
+
+	$('#img12').hover(
+			function() {
+					$('#btn12').show();
+			},function() {
+					$('#btn12').hide();
+			}
+	);
+
+	$('#img13').hover(
+			function() {
+					$('#btn13').show();
+			},function() {
+					$('#btn13').hide();
+			}
+	);
+
+	$('#img14').hover(
+			function() {
+					$('#btn14').show();
+			},function() {
+					$('#btn14').hide();
+			}
+	);
+
+	$('#img15').hover(
+			function() {
+					$('#btn15').show();
+			},function() {
+					$('#btn15').hide();
+			}
+	);
 	// try uncommenting (deleting the // before a line) to see its effects
 
 	//$('.item').addClass('shrink');
