53373bcc30c9fec48b17e8746e5f8f58e148238f
gloria <Gloriab@MacBook-Pro-3.local>
2018-03-08 11:39:51 -0800

    j

--- folder/step7/js/script.js
+++ folder/step7/js/script.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
 
 
+	$('body').mousemove(function(e){
+		$('#position').html('Position X : ' + e.pageX + '<br />Position Y : ' + e.pageY);
+		// try switching the 'top' and 'left' values
+		$('#position').css({ 'top': e.pageY+'px', 'left': e.pageX+'px' });
+	});
+});
+
+
 
 	$('#img1').hover(
 	    function() {
@@ -122,11 +130,6 @@ $(document).ready(function(){
 			}
 	);
 
-	$("#btn").hover(function() {
-	    $(this).css('cursor','1.png');
-	}, function() {
-	    $(this).css('cursor','auto');
-	});
 
 
 
