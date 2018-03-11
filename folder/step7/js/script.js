e29fbba43185371fb4c065db10114f54b5e04616
gloria <Gloriab@MacBook-Pro-3.local>
2018-03-11 16:35:04 -0700

    e

--- folder/step7/js/script.js
+++ folder/step7/js/script.js
@@ -1,134 +1,155 @@
 $(document).ready(function(){
 
-
+	var button = document.getElementById("counting"),
+	count=0;
+	button.onmouseenter = function(){
+		count+=1;
+		button.innerHTML = "Cursors Found" + count;
+	}
 
 	$('#img1').hover(
 	    function() {
 	        $('#btn').show();
-	    },function() {
-	        $('#btn').hide();
 	    }
+
+			// ,function() {
+	    //     $('#btn').hide();
+	    // }
 	);
 
 	$('#img2').hover(
 			function() {
 					$('#btn2').show();
-			},function() {
-					$('#btn2').hide();
 			}
+			// ,function() {
+			// 		$('#btn2').hide();
+			// }
 	);
 
 	$('#img3').hover(
 			function() {
 					$('#btn3').show();
-			},function() {
-					$('#btn3').hide();
 			}
+			// ,function() {
+			// 		$('#btn3').hide();
+			// }
 	);
 
 	$('#img4').hover(
 			function() {
 					$('#btn4').show();
-			},function() {
-					$('#btn4').hide();
 			}
+			// ,function() {
+			// 		$('#btn4').hide();
+			// }
 	);
 
 	$('#img5').hover(
 			function() {
 					$('#btn5').show();
-			},function() {
-					$('#btn5').hide();
 			}
+			// ,function() {
+			// 		$('#btn5').hide();
+			// }
 	);
 
 	$('#img6').hover(
 			function() {
 					$('#btn6').show();
-			},function() {
-					$('#btn6').hide();
 			}
+			// ,function() {
+			// 		$('#btn6').hide();
+			// }
 	);
 
 	$('#img7').hover(
 			function() {
 					$('#btn7').show();
-			},function() {
-					$('#btn7').hide();
 			}
+			// ,function() {
+			// 		$('#btn7').hide();
+			// }
 	);
 
 	$('#img8').hover(
 			function() {
 					$('#btn8').show();
-			},function() {
-					$('#btn8').hide();
 			}
+			// ,function() {
+			// 		$('#btn8').hide();
+			// }
 	);
 
 	$('#img9').hover(
 			function() {
 					$('#btn9').show();
-			},function() {
-					$('#btn9').hide();
 			}
+			// ,function() {
+			// 		$('#btn9').hide();
+			// }
 	);
 
 	$('#img10').hover(
 			function() {
 					$('#btn10').show();
-			},function() {
-					$('#btn10').hide();
 			}
+			// ,function() {
+			// 		$('#btn10').hide();
+			// }
 	);
 
 	$('#img11').hover(
 			function() {
 					$('#btn11').show();
-			},function() {
-					$('#btn11').hide();
 			}
+			// ,function() {
+			// 		$('#btn11').hide();
+			// }
 	);
 
 	$('#img12').hover(
 			function() {
 					$('#btn12').show();
-			},function() {
-					$('#btn12').hide();
 			}
+			// ,function() {
+			// 		$('#btn12').hide();
+			// }
 	);
 
 	$('#img13').hover(
 			function() {
 					$('#btn13').show();
-			},function() {
-					$('#btn13').hide();
 			}
+			// ,function() {
+			// 		$('#btn13').hide();
+			// }
 	);
 
 	$('#img14').hover(
 			function() {
 					$('#btn14').show();
-			},function() {
-					$('#btn14').hide();
 			}
+			// ,function() {
+			// 		$('#btn14').hide();
+			// }
 	);
 
 	$('#img15').hover(
 			function() {
 					$('#btn15').show();
-			},function() {
-					$('#btn15').hide();
 			}
+			// ,function() {
+			// 		$('#btn15').hide();
+			// }
 	);
 
-	$(document).ready(function(){
-		$('body').mousemove(function(e){
-			$('.position').html('Position X : ' + e.pageX + '<br />Position Y : ' + e.pageY);
-			// try switching the 'top' and 'left' values
-			$('.position').css({ 'top': e.pageY+'px', 'left': e.pageX+'px' });
-		});
-	});
+	// $(document).ready(function(){
+	// 	$('body').mousemove(function(e){
+	// 		$('.position').html('Position X : ' + e.pageX + '<br />Position Y : ' + e.pageY);
+	// 		// try switching the 'top' and 'left' values
+	// 		$('.position').css({ 'top': e.pageY+'px', 'left': e.pageX+'px' });
+	// 	});
+	// });
 
 	// try uncommenting (deleting the // before a line) to see its effects
 
