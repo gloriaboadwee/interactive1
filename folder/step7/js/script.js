aeb2eecded2edc572d0f8c61cce2bc3f457dff96
gloria <Gloriab@MacBook-Pro-3.local>
2018-03-12 23:27:31 -0700

    6

--- folder/step7/js/script.js
+++ folder/step7/js/script.js
@@ -6,15 +6,14 @@ $(document).ready(function(){
 	count=0;
 	button.onmouseenter = function(){
 		count+=1;
-		button.innerHTML = "Cursors Found /15: " + count;
+		button.innerHTML = "Cursors Found  /15: " + count;
 	}
-
 
 
 	$('#img1').hover(
 	    function() {
 	        $('#btn').show();
-					    document.body.style.background = "#f3f3f3 url('gif/1.gif') repeat right top";
+					    document.body.style.background = "#f3f3f3 url('gif/blue.gif') repeat right top";
 	    }
 			,function() {
 	        $(this).css("background", "none");
@@ -34,119 +33,132 @@ $(document).ready(function(){
 	$('#img3').hover(
 			function() {
 					$('#btn3').show();
-			}
-			// ,function() {
-			// 		$('#btn3').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/3.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img4').hover(
 			function() {
 					$('#btn4').show();
-			}
-			// ,function() {
-			// 		$('#btn4').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/222.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img5').hover(
 			function() {
 					$('#btn5').show();
-			}
-			// ,function() {
-			// 		$('#btn5').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/666.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img6').hover(
 			function() {
 					$('#btn6').show();
-			}
-			// ,function() {
-			// 		$('#btn6').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/77.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img7').hover(
 			function() {
 					$('#btn7').show();
-			}
-			// ,function() {
-			// 		$('#btn7').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/66.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img8').hover(
 			function() {
 					$('#btn8').show();
-			}
-			// ,function() {
-			// 		$('#btn8').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/11.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img9').hover(
 			function() {
 					$('#btn9').show();
-			}
-			// ,function() {
-			// 		$('#btn9').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/movedots.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img10').hover(
 			function() {
 					$('#btn10').show();
-			}
-			// ,function() {
-			// 		$('#btn10').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/12.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img11').hover(
 			function() {
 					$('#btn11').show();
-			}
-			// ,function() {
-			// 		$('#btn11').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/345.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img12').hover(
 			function() {
 					$('#btn12').show();
-			}
-			// ,function() {
-			// 		$('#btn12').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/red.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img13').hover(
 			function() {
 					$('#btn13').show();
-			}
-			// ,function() {
-			// 		$('#btn13').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/13.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img14').hover(
 			function() {
 					$('#btn14').show();
-			}
-			// ,function() {
-			// 		$('#btn14').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/15.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	$('#img15').hover(
 			function() {
 					$('#btn15').show();
-			}
-			// ,function() {
-			// 		$('#btn15').hide();
-			// }
-	);
+					document.body.style.background = "#f3f3f3 url('gif/14.gif') repeat right top";
+		 }
+		 ,function() {
+					$(this).css("background", "none");
+		 }
+ );
 
 	// $(document).ready(function(){
 	// 	$('body').mousemove(function(e){
