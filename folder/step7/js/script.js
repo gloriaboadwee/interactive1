928a53495f094ee42a916e6759c906634aafebab
gloria <Gloriab@MacBook-Pro-3.local>
2018-03-13 16:39:01 -0700

    m

--- folder/step7/js/script.js
+++ folder/step7/js/script.js
@@ -1,27 +1,37 @@
 $(document).ready(function(){
 
-
-
-	var button = document.getElementById("count"),
-	count=0;
-	button.onmouseenter = function(){
-		count+=1;
-		button.innerHTML = "Cursors Found  /15: " + count;
-	}
-	// if count == 15{
-	// 	$('#end').click(
-	// 			function() {
-	// 					$('#btn');
-	// 							document.body.style.background = "#f3f3f3 url('gif/end1.gif') center";
-	// 			}
+document.body.style.cursor = 'none';
   //
+	// var button = document.getElementById("count"),
+	// count=0;
+	// button.onmouseenter = function(){
+	// 	count+=1;
+	// 	button.innerHTML = "Cursors Found  /15: " + count;
 	// }
+
+
+	// if (n > 15) {
+	// 	$('#end').click()
+	// 			(function(){
+	// 						document.body.style.background = "#f3f3f3 url('gif/end1.gif') center";
+	// });
+
+	var n = 0;
+
+	$('.hover').mouseenter(function(){
+		$(this).removeClass('hover');
+		n = n+1;
+		$('.number').html(n);
+		// button.innerHTML = "Cursors Found  /15: " + n;
+	});
 
 	$('#img1').hover(
 	    function() {
 	        $('#btn').show();
 					    document.body.style.background = "#f3f3f3 url('gif/blue.gif') repeat right top";
-	    }
+							$('.position').html('<img src="img/1.png" />');
+
+			}
 			,function() {
 	        $(this).css("background", "none");
 	    }
@@ -31,6 +41,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn2').show();
 					 document.body.style.background = "#f3f3f3 url('gif/2.gif') repeat right top";
+					 $('.position').html('<img src="img/2.png" />');
 			}
 			,function() {
 					 $(this).css("background", "none");
@@ -41,6 +52,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn3').show();
 					document.body.style.background = "#f3f3f3 url('gif/3.gif') repeat right top";
+					$('.position').html('<img src="img/hand3.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -51,6 +63,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn4').show();
 					document.body.style.background = "#f3f3f3 url('gif/222.gif') repeat right top";
+					$('.position').html('<img src="img/5.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -61,6 +74,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn5').show();
 					document.body.style.background = "#f3f3f3 url('gif/666.gif') repeat right top";
+					$('.position').html('<img src="img/hand4.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -71,6 +85,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn6').show();
 					document.body.style.background = "#f3f3f3 url('gif/77.gif') repeat right top";
+					$('.position').html('<img src="img/77.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -81,6 +96,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn7').show();
 					document.body.style.background = "#f3f3f3 url('gif/66.gif') repeat right top";
+					$('.position').html('<img src="img/222.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -91,6 +107,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn8').show();
 					document.body.style.background = "#f3f3f3 url('gif/11.gif') repeat right top";
+					$('.position').html('<img src="img/444.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -101,6 +118,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn9').show();
 					document.body.style.background = "#f3f3f3 url('gif/movedots.gif') repeat right top";
+					$('.position').html('<img src="img/66.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -111,6 +129,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn10').show();
 					document.body.style.background = "#f3f3f3 url('gif/12.gif') repeat right top";
+					$('.position').html('<img src="img/hand.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -121,6 +140,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn11').show();
 					document.body.style.background = "#f3f3f3 url('gif/345.gif') repeat right top";
+					$('.position').html('<img src="img/hand2.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -131,6 +151,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn12').show();
 					document.body.style.background = "#f3f3f3 url('gif/red.gif') repeat right top";
+					$('.position').html('<img src="img/3.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -141,6 +162,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn13').show();
 					document.body.style.background = "#f3f3f3 url('gif/13.gif') repeat right top";
+					$('.position').html('<img src="img/11.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -151,6 +173,7 @@ $(document).ready(function(){
 			function() {
 					$('#btn14').show();
 					document.body.style.background = "#f3f3f3 url('gif/15.gif') repeat right top";
+					$('.position').html('<img src="img/16.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
@@ -161,11 +184,19 @@ $(document).ready(function(){
 			function() {
 					$('#btn15').show();
 					document.body.style.background = "#f3f3f3 url('gif/14.gif') repeat right top";
+					$('.position').html('<img src="img/666.png" />');
 		 }
 		 ,function() {
 					$(this).css("background", "none");
 		 }
+
  );
+
+ 	$('body').mousemove(function(e){
+ 		//$('.position').html('Position X : ' + e.pageX + '<br />Position Y : ' + e.pageY);
+ 		// try switching the 'top' and 'left' values
+ 		$('.position').css({ 'top': e.pageY+'px', 'left': e.pageX+'px' });
+ 	});
 
 	// $(document).ready(function(){
 	// 	$('body').mousemove(function(e){
