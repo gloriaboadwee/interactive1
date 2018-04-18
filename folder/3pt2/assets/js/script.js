cd0707495c40cbf6e0884f9b5eb4f4bfa54fe2a3
gloria <Gloriab@MacBook-Pro-3.local>
2018-04-17 17:53:10 -0700

    m

--- folder/3pt2/assets/js/script.js
+++ folder/3pt2/assets/js/script.js
@@ -1,22 +1,16 @@
 $(document).ready(function(){
 
 	n = 1;
-	var items = Array('/','|','-','+','=');
 
-	$('body').on( "mousemove", function(e) {
-		// move #position on mouse move
-		$('#position').css({ 'top': e.pageY+'px', 'left': e.pageX+'px' });
-		// copy element on mousemove
-		var clone = $('#position').clone();
-		$('.page-'+n).append(clone);
-	});
+	setInterval(function(){
+		$('body').append('<img src="img/circle.png" />');
 
-	$('body').click(function(){
+		if (n%10 == 0) {
+			$('body').append('<div class="break"></div>');
+		}
+
 		n++;
-		var item = items[Math.floor(Math.random()*items.length)];
-		$('#position').html(item);
-		$('body').append('<div class="break"></div><div class="page-'+n+'"></div>');
-	});
 
+	},1000);
 
 });
