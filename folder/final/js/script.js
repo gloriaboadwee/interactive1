b80a80dfbd8c39b8e416a09806a598c9e1fe4268
gloria <Gloriab@MacBook-Pro-3.local>
2018-04-24 11:52:43 -0700

    1

--- folder/final/js/script.js
+++ folder/final/js/script.js
@@ -1,30 +1,14 @@
-$(document).ready(function(){
-
-$('img').click(function(){
-    // img append random number
-    var path = 'img/',
-    imgs = ['popup1.jpeg','popup4.jpeg','popup3.jpeg'],
-    i = Math.floor(Math.random()*imgs.length);
-    $('.el').append("<img src='"+path+imgs[i]+"'>").hide().fadeIn(2000);
-});
-function randomImg1() {
-    var Images1 = new Array();
-    Images1[1] = "img/popup1.jpeg";
-    Images1[2] = "img/popup3.jpeg";
-    Images1[3] = "img/popup4.jpeg";
-    var rnd = Math.floor(Math.random() * myImages1.length);
-    if (rnd == 0) {
-            rnd = 1;
-    }
-    document.write('<img class="who" src="' + myImages1[rnd] + '">');
-}
-// function displayimage()
-// {
-//   alert('hi');
-//   var images=[{
-//     src: "img/popup1.jpeg"
-//     src: "img/popup2.jpeg"
-//   }]
-// }
-});
-var random_images_array = ["popup1.jpeg", "popup3.jpeg"];
+// $(document).ready(function(){
+//   //
+//   // $("#clickable").click(function(){
+//   //
+//   // var random_images_array = ['popup1.jpeg', 'popup2.jpeg', 'popup3.jpeg', 'popup4.jpeg', 'popup5.jpeg', 'popup6.jpeg'];
+//   //
+//   // function getRandomImage(imgAr, path) {
+//   //     path = path || 'images/';
+//   //     var num = Math.floor( Math.random() * imgAr.length );
+//   //     var img = imgAr[ num ];
+//   //     var imgStr = '<img src="' + path + img + '" alt = "">';
+//   //     document.write(imgStr); document.close();
+//   //   }
+//   }
